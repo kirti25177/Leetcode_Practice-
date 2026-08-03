2ff6d61b43fdf2f03b951075974d2665a533fbe0
Kirti <kirti25177@gmail.com>
2026-08-03 22:10:19 +0530

    Time: 0 ms (100%), Space: 23.8 MB (56.58%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,6 +4,7 @@ public:
         int n=nums.size();
         int j=0;
         for(int i=0;i<n;i++){
+            //We are traversing here
             if(nums[i]!=0){
                 swap(nums[i],nums[j]);
                 j++;
