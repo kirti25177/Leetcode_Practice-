b240390820445b5c91f3d785100c884927dfa402
Kirti <kirti25177@gmail.com>
2026-08-21 18:00:55 +0530

    Time: 0 ms (100%), Space: 23.7 MB (98.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,8 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
         int j=0;
-        for(int i=0;i<n;i++){
-            //We are traversing here
+        for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
                 swap(nums[i],nums[j]);
                 j++;
