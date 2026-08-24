ebbf5394b33dea32573ecf7949201001e94efc71
Kirti <kirti25177@gmail.com>
2026-08-24 23:16:16 +0530

    Time: 0 ms (100%), Space: 11.6 MB (79.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -6,9 +6,9 @@ public:
         int high=nums.size()-1;
         while(mid<=high){
             if(nums[mid]==0){
-                swap(nums[low],nums[mid]);
-                low++;
+                swap(nums[mid],nums[low]);
                 mid++;
+                low++;
             }
             else if(nums[mid]==1){
                 mid++;
