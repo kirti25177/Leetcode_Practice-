1a3b83e210db5a36cb2b8b185ef27e182b4e6f65
Kirti <kirti25177@gmail.com>
2026-08-24 12:12:13 +0530

    Time: 0 ms (100%), Space: 24.7 MB (14.79%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
+        //Optimal solution using Two Pointers
+        //Time complexity for this O(nlogn)
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=nums.size()-1;
         int count=0;
-        //This is brute force solution which takes the time complexity of O(n^2)
-        //We can optimise the solution by using two pointer approach
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]<target){
-                    count++;
-                }
+        while(i<j){
+            if(nums[i]+nums[j]<target){
+                count+=j-i;
+                i++;
+            }
+            else{
+                j--;
             }
         }
         return count;
